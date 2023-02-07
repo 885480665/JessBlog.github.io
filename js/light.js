e0714b4d5218e332eb021dcf47683e1458c4a6f3
885480665 <885480665@qq.com>
2023-02-07 22:10:26 +0800

    Site updated: 2023-02-07 22:10:26

---
+++ js/light.js
@@ -0,0 +1,49 @@
+// �޺��Ч��
+// ��ɫ����
+var arr = ["#39c5bb", "#f14747", "#f1a247", "#f1ee47", "#b347f1", "#1edbff", "#ed709b", "#5636ed"];
+// ��ɫ����
+var idx = 0;
+
+// �л���ɫ
+function changeColor() {
+    // ��ҹ��ģʽ������
+    if (document.getElementsByTagName('html')[0].getAttribute('data-theme') == 'dark') {
+        if (document.getElementById("site-name"))
+            document.getElementById("site-name").style.textShadow = arr[idx] + " 0 0 15px";
+        if (document.getElementById("site-title"))
+            document.getElementById("site-title").style.textShadow = arr[idx] + " 0 0 15px";
+        if (document.getElementById("site-subtitle"))
+            document.getElementById("site-subtitle").style.textShadow = arr[idx] + " 0 0 10px";
+        if (document.getElementById("post-info"))
+            document.getElementById("post-info").style.textShadow = arr[idx] + " 0 0 5px";
+        try {
+            document.getElementsByClassName("author-info__name")[0].style.textShadow = arr[idx] + " 0 0 12px";
+            document.getElementsByClassName("author-info__description")[0].style.textShadow = arr[idx] + " 0 0 12px";
+        } catch {
+
+        }
+        idx++;
+        if (idx == 8) {
+            idx = 0;
+        }
+    } else {
+        // ����ģʽ�ָ�Ĭ��
+        if (document.getElementById("site-name"))
+            document.getElementById("site-name").style.textShadow = "#1e1e1ee0 1px 1px 1px";
+        if (document.getElementById("site-title"))
+            document.getElementById("site-title").style.textShadow = "#1e1e1ee0 1px 1px 1px";
+        if (document.getElementById("site-subtitle"))
+            document.getElementById("site-subtitle").style.textShadow = "#1e1e1ee0 1px 1px 1px";
+        if (document.getElementById("post-info"))
+            document.getElementById("post-info").style.textShadow = "#1e1e1ee0 1px 1px 1px";
+        try {
+            document.getElementsByClassName("author-info__name")[0].style.textShadow = "";
+            document.getElementsByClassName("author-info__description")[0].style.textShadow = "";
+        } catch {
+
+        }
+    }
+}
+
+// ������ʱ��
+window.onload = setInterval(changeColor, 1200);
