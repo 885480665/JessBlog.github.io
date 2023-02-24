ce1f2b8b3f4744d8db8b770c325c0b670d073203
885480665 <885480665@qq.com>
2023-02-24 20:50:18 +0800

    Site updated: 2023-02-24 20:50:17

--- js/txmap.js
+++ js/txmap.js
@@ -9,11 +9,15 @@ $.ajax({
     dataType: 'jsonp',
     success: function (res) {
         ipLoacation = res;
+        showWelcome();
     }
 })
+
+//根据经纬度计算两点距离(点1经度,点1纬度,点2经度,点2纬度)
 function getDistance(e1, n1, e2, n2) {
     const R = 6371
     const { sin, cos, asin, PI, hypot } = Math
+
     let getPoint = (e, n) => {
         e *= PI / 180
         n *= PI / 180
@@ -27,19 +31,22 @@ function getDistance(e1, n1, e2, n2) {
     return Math.round(r);
 }
 
+//根据自己的需求定制
 function showWelcome() {
+    if (!document.getElementById("welcome-info")) return
 
-    let dist = getDistance(113.34499552, 23.15537143, ipLoacation.result.location.lng, ipLoacation.result.location.lat); //这里换成自己的经纬度
+    let dist = getDistance(120.16520, 31.90875, ipLoacation.result.location.lng, ipLoacation.result.location.lat); //这里记录你自己的经纬度
+
     let pos = ipLoacation.result.ad_info.nation;
-    let ip;
     let posdesc;
     //根据国家、省份、城市信息自定义欢迎语
+    //海外地区不支持省份及城市信息
     switch (ipLoacation.result.ad_info.nation) {
         case "日本":
             posdesc = "よろしく，一起去看樱花吗";
             break;
         case "美国":
-            posdesc = "Let us live in peace!";
+            posdesc = "Make America Great Again!";
             break;
         case "英国":
             posdesc = "想同你一起夜乘伦敦眼";
@@ -60,14 +67,19 @@ function showWelcome() {
             posdesc = "拾起一片枫叶赠予你";
             break;
         case "中国":
-            pos = ipLoacation.result.ad_info.province + " " + ipLoacation.result.ad_info.city + " " + ipLoacation.result.ad_info.district;
-            ip = ipLoacation.result.ip;
+            pos = ipLoacation.result.ad_info.province + " " + ipLoacation.result.ad_info.city;
             switch (ipLoacation.result.ad_info.province) {
                 case "北京市":
+                    pos = "北京市";
                     posdesc = "北——京——欢迎你~~~";
                     break;
                 case "天津市":
+                    pos = "天津市";
                     posdesc = "讲段相声吧。";
+                    break;
+                case "重庆市":
+                    pos = "重庆市";
+                    posdesc = "高德地图:已到达重庆，下面交给百度地图导航。"
                     break;
                 case "河北省":
                     posdesc = "山势巍巍成壁垒，天下雄关。铁马金戈由此向，无限江山。";
@@ -88,15 +100,19 @@ function showWelcome() {
                     posdesc = "很喜欢哈尔滨大剧院。";
                     break;
                 case "上海市":
+                    pos = "上海市";
                     posdesc = "众所周知，中国只有两个城市。";
                     break;
                 case "江苏省":
                     switch (ipLoacation.result.ad_info.city) {
                         case "南京市":
-                            posdesc = "这是我挺想去的城市啦。";
+                            posdesc = "欢迎来自安徽省南京市的小伙伴。";
                             break;
                         case "苏州市":
                             posdesc = "上有天堂，下有苏杭。";
+                            break;
+                        case "泰州市":
+                            posdesc = "这里也是我的故乡。";
                             break;
                         default:
                             posdesc = "散装是必须要散装的。";
@@ -105,28 +121,6 @@ function showWelcome() {
                     break;
                 case "浙江省":
                     posdesc = "东风渐绿西湖柳，雁已还人未南归。";
-                    break;
-                case "河南省":
-                    switch (ipLoacation.result.ad_info.city) {
-                        case "郑州市":
-                            posdesc = "豫州之域，天地之中。";
-                            break;
-                        case "南阳市":
-                            posdesc = "臣本布衣，躬耕于南阳。此南阳非彼南阳！";
-                            break;
-                        case "驻马店市":
-                            posdesc = "峰峰有奇石，石石挟仙气。嵖岈山的花很美哦！";
-                            break;
-                        case "开封市":
-                            posdesc = "刚正不阿包青天。";
-                            break;
-                        case "洛阳市":
-                            posdesc = "洛阳牡丹甲天下。";
-                            break;
-                        default:
-                            posdesc = "可否带我品尝河南烩面啦？";
-                            break;
-                    }
                     break;
                 case "安徽省":
                     posdesc = "蚌埠住了，芜湖起飞。";
@@ -186,13 +180,15 @@ function showWelcome() {
                     posdesc = "我在这头，大陆在那头。";
                     break;
                 case "香港特别行政区":
+                    pos = "香港特别行政区";
                     posdesc = "永定贼有残留地鬼嚎，迎击光非岁玉。";
                     break;
                 case "澳门特别行政区":
+                    pos = "澳门特别行政区";
                     posdesc = "性感荷官，在线发牌。";
                     break;
                 default:
-                    posdesc = "带我去你的城市逛逛吧！";
+                    posdesc = "社会主义大法好。";
                     break;
             }
             break;
@@ -201,16 +197,16 @@ function showWelcome() {
             break;
     }
 
-    //根据本地时间切换欢迎语
+    //判断时间
     let timeChange;
     let date = new Date();
-    if (date.getHours() >= 5 && date.getHours() < 11) timeChange = "<span>上午好</span>，一日之计在于晨！";
-    else if (date.getHours() >= 11 && date.getHours() < 13) timeChange = "<span>中午好</span>，该摸鱼吃午饭了。";
+    if (date.getHours() >= 5 && date.getHours() < 11) timeChange = "<span>上午好</span>，一日之计在于晨";
+    else if (date.getHours() >= 1 && date.getHours() < 13) timeChange = "<span>中午好</span>，该摸鱼吃午饭了";
     else if (date.getHours() >= 13 && date.getHours() < 15) timeChange = "<span>下午好</span>，懒懒地睡个午觉吧！";
-    else if (date.getHours() >= 15 && date.getHours() < 16) timeChange = "<span>三点几啦</span>，一起饮茶呀！";
+    else if (date.getHours() >= 15 && date.getHours() < 16) timeChange = "<span>三点几啦</span>，饮茶先啦！";
     else if (date.getHours() >= 16 && date.getHours() < 19) timeChange = "<span>夕阳无限好！</span>";
     else if (date.getHours() >= 19 && date.getHours() < 24) timeChange = "<span>晚上好</span>，夜生活嗨起来！";
-    else timeChange = "夜深了，早点休息，少熬夜。";
+    else timeChange = "夜深了，早点休息，少熬夜";
 
     try {
         //自定义文本和需要放的位置
